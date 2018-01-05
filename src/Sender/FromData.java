ed2dc696b36a9ae04c564316165d4ab5ac4fedf9
CevG33ko <gab.cevallos@gmail.com>
2018-01-05 22:24:24 +0100

    Anfang des Einlesen der Datei. Dient als Orientierung zum schreiben der Datei

--- src/Sender/FromData.java
+++ src/Sender/FromData.java
@@ -1,4 +1,27 @@
 package Sender;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class FromData {
+
+    //TODO Als Input Argument bei Programmstart
+
+    private static final String FILENAME = "E:\\test\\filename.txt";
+
+    public static void main(String[] args) {
+
+        File file = new File(FILENAME);
+        try (FileInputStream fin = new FileInputStream(file)) {
+
+            byte fileContent[] = new byte[(int) file.length()];
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found" + e);
+        } catch (IOException ioe) {
+            System.out.println("Exception while reading file" + ioe);
+        }
+    }
 }
